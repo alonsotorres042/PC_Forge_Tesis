9cb8f4e2e8b650f5d443c63b302b8c1d45bb03e4
alonsotorres042 <alonso.torres@tecsup.edu.pe>
2025-11-26 17:50:55 -0500

    Old stuff X new attach system

--- Assets/ProjectAssets/Scripts/BaseComponent/ComponentData.cs
+++ Assets/ProjectAssets/Scripts/BaseComponent/ComponentData.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ComponentType { Cooler, CPU, GPU, Motherboard, RAM, ThermalPaste }
+public enum ComponentType { Cooler, CPU, GPU, Motherboard, RAM, ThermalPaste, PowerSupply }
 [CreateAssetMenu(fileName = "ComponentData", menuName = "Scriptable Objects/ComponentData")]
 public class ComponentData : ScriptableObject
 {
